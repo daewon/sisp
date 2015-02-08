22c34551b8f2c261f893ed0da17ead3badeb910d
daewon <blueiur@gmail.com>
2015-02-08 17:04:07 +0900

    테스트 살림

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -3,83 +3,83 @@ import org.scalatest.FunSuite
 class SispSpec extends FunSuite {
   import com.daewon.sisp.Sisp._
 
-  // test("create symbol") {
-  //   assert(Integer(1).value == 1)
-  //   assert(Symbol("dun").value == "dun")
-  //   assert(nil == nil)
-  //   val sum = BuiltIn { _args: Atom =>
-  //     def sum(args: Atom): Int = args match {
-  //       case Pair(Integer(n), tl) => n + sum(tl)
-  //       case `nil` => 0
-  //     }
-  //     Integer(sum(_args))
-  //   }
-  // }
+  import Environment._
+  import Helpers._
 
-  // test("list") {
-  //   val a = Symbol("a")
-  //   val b = Symbol("b")
-  //   val c = Integer(10)
-  //   assert(list(a, b) == Pair(a, Pair(b, nil)))
-  //   assert(list(a, b, c) == Pair(a, Pair(b, Pair(c, nil))))
-  // }
+  test("create symbol") {
+    assert(Integer(1).value == 1)
+    assert(Sym('dun).value == 'dun)
+    assert(nil == nil)
 
-  // test("show") {
-  //   assert(show(Pair(Integer(1), nil)) == "(1)")
-  //   assert(show(Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), nil)))) == "(1 2 3)")
-  //   assert(show(Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), Pair(Integer(4), nil))))) == "(1 2 3 4)")
+    val sum = BuiltIn { _args: Atom =>
+      def sum(args: Atom): Int = args match {
+        case Pair(Integer(n), tl) => n + sum(tl)
+        case `nil` => 0
+      }
+      Integer(sum(_args))
+    }
+  }
 
-  //   assert(show(Pair(Pair(Integer(1), nil), nil)) == "((1))")
-  //   assert(show(Pair(Pair(Integer(1), nil), Pair(Integer(2), nil))) == "((1) 2)")
+  test("list") {
+    assert(list('a, 'b) == Pair(Sym('a), Pair(Sym('b), nil)))
+    assert(list('a, 'b, 'c) == Pair(Sym('a), Pair(Sym('b), Pair(Sym('c), nil))))
+  }
 
-  //   val lst123 = Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), nil)))
-  //   assert(show(Pair(Symbol("foo"), lst123)) == "(foo 1 2 3)")
-  //   assert(show(Pair(Symbol("foo"), Pair(lst123, nil))) == "(foo (1 2 3))")
+  test("show") {
+    assert(show(Pair(Integer(1), nil)) == "(1)")
+    assert(show(Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), nil)))) == "(1 2 3)")
+    assert(show(Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), Pair(Integer(4), nil))))) == "(1 2 3 4)")
 
-  //   assert(show(Pair(Integer(1), Integer(2))) == "(1 . 2)")
-  //   assert(show(Pair(Integer(1), Pair(Integer(2), nil))) == "(1 2)")
+    assert(show(Pair(Pair(Integer(1), nil), nil)) == "((1))")
+    assert(show(Pair(Pair(Integer(1), nil), Pair(Integer(2), nil))) == "((1) 2)")
 
-  //   assert(show(Pair(Symbol("dun"), Pair(Integer(1), nil))) == "(dun 1)")
-  //   assert(show(Pair(Pair(Symbol("dun"), Integer(1)), Integer(2))) == "((dun . 1) . 2)")
+    val lst123 = Pair(Integer(1), Pair(Integer(2), Pair(Integer(3), nil)))
+    assert(show(Pair(Sym('foo), lst123)) == "(foo 1 2 3)")
+    assert(show(Pair(Sym('foo), Pair(lst123, nil))) == "(foo (1 2 3))")
 
-  //   val a = Pair(Symbol("a"), Integer(10))
-  //   val b = Pair(Symbol("b"), Integer(20))
-  //   val c = Pair(Symbol("c"), Integer(30))
-  //   val d = Pair(Symbol("d"), Integer(40))
+    assert(show(Pair(Integer(1), Integer(2))) == "(1 . 2)")
+    assert(show(Pair(Integer(1), Pair(Integer(2), nil))) == "(1 2)")
 
-  //   assert(show(Pair(a, Pair(b, Pair(c, Pair(d, nil))))) == "((a . 10) (b . 20) (c . 30) (d . 40))")
-  //   val lambda = Closure(nil,
-  //     Pair(Symbol("a"), Pair(Symbol("b"), nil)),
-  //     Pair(Symbol("+"), Pair(Symbol("a"), Pair(Symbol("b"), nil)))
-  //   )
-  //   assert(show(lambda) == "(lambda (a b) (+ a b))")
-  //   assert(show(BuiltIn{_ => nil}) == "built-in <function1>")
+    assert(show(Pair(Sym('dun), Pair(Integer(1), nil))) == "(dun 1)")
+    assert(show(Pair(Pair(Sym('dun), Integer(1)), Integer(2))) == "((dun . 1) . 2)")
 
-  //   val lambdaPair =
-  //     Pair(Symbol("a"), list(Symbol("lambda"), list(Symbol("a")), Symbol("a")))
-  //   // special case for lambda pair
-  //   assert(show(lambdaPair) == "(a . (lambda (a) a))")
-  // }
+    val a = Pair(Sym('a), Integer(10))
+    val b = Pair(Sym('b), Integer(20))
+    val c = Pair(Sym('c), Integer(30))
+    val d = Pair(Sym('d), Integer(40))
 
-  // test("predicate") {
-  //   // nil
-  //   assert(nilp(nil))
+    assert(show(Pair(a, Pair(b, Pair(c, Pair(d, nil))))) == "((a . 10) (b . 20) (c . 30) (d . 40))")
+    val lambda = Closure(nil,
+      Pair(Sym('a), Pair(Sym('b), nil)),
+      Pair(Sym('+), Pair(Sym('a), Pair(Sym('b), nil)))
+    )
 
-  //   // (1 . 2)
-  //   assert(pairp(Pair(Integer(1), Integer(2))))
+    assert(show(lambda) == "(lambda (a b) (+ a b))")
+    assert(show(BuiltIn{_ => nil}) == "built-in <function1>")
 
-  //   // (1 2)
-  //   assert(listp(Pair(Integer(1), Integer(2))) == false)
+    val lambdaPair =
+      Pair(Sym('a), list(Sym('lambda), list(Sym('a)), Sym('a)))
 
-  //   // ((dun . 1) . 2)
-  //   assert(pairp(Pair(Pair(Symbol("dun"), Integer(1)), Integer(2))))
-  //   assert(listp(Pair(Pair(Symbol("dun"), Integer(1)), Integer(2))) == false)
-  // }
+    // special case for lambda pair
+    assert(show(lambdaPair) == "(a . (lambda (a) a))")
+  }
+
+  test("predicate") {
+    // nil
+    assert(nilp(nil))
+
+    // (1 . 2)
+    assert(pairp(Pair(Integer(1), Integer(2))))
+
+    // (1 2)
+    assert(listp(Pair(Integer(1), Integer(2))) == false)
+
+    // ((dun . 1) . 2)
+    assert(pairp(Pair(Pair(Symbol("dun"), Integer(1)), Integer(2))))
+    assert(listp(Pair(Pair(Symbol("dun"), Integer(1)), Integer(2))) == false)
+  }
 
   test("env") {
-    import Environment._
-    import Helpers._
-
     // get
     var parent = createEnv()
     parent = set(parent, 'a, 10)
@@ -98,103 +98,96 @@ class SispSpec extends FunSuite {
   }
 
   test("closure") {
-    // import Environment._
-    // import Helpers._
+    var env = createEnv()
+    var ret: Atom = nil
+    var value: Atom = nil
 
-    // var env = createEnv()
-    // var ret: Atom = nil
-    // var value: Atom = nil
+    val add = BuiltIn { _args: Atom =>
+      def sum(args: Atom): Int = args match {
+        case Pair(Integer(n), tl) => n + sum(tl)
+        case `nil` => 0
+      }
+      Integer(sum(_args))
+    }
 
-    // val add = BuiltIn { _args: Atom =>
-    //   def sum(args: Atom): Int = args match {
-    //     case Pair(Integer(n), tl) => n + sum(tl)
-    //     case `nil` => 0
-    //   }
-    //   Integer(sum(_args))
-    // }
+    env = set(env, 'age, 10)
+    env = set(env, '+, add)
 
-    // env = set(env, 'age, 10)
-    // env = set(env, '+, add)
+    val expr = list('+, 1, 'age)
+    ret = eval(env, expr)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(11))
 
-    // val expr = list('+, 1, 'age)
-    // ret = eval(env, expr)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(11))
+    val cls = Closure(createEnv(env), list('a, 'b), list('+, 'a, 'b))
+    ret = eval(env, list(cls, 1, 2))
+    env = car(ret)
+    value = cdr(ret)
 
-    // val cls = Closure(createEnv(env), list('a, 'b), list('+, 'a, 'b))
-    // ret = eval(env, list(cls, 1, 2))
-    // env = car(ret)
-    // value = cdr(ret)
-
-    // assert(value == Integer(3))
+    assert(value == Integer(3))
   }
 
   test("eval") {
-    import Environment._
-    import Helpers._
-
     var env = createEnv()
     var value: Atom = nil
     var ret: Atom = nil
     var exp: Atom = nil
 
-    // // (define a 10) == a
-    // exp = list('define, 'a, 10)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Sym('a))
+    // (define a 10) == a
+    exp = list('define, 'a, 10)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Sym('a))
 
-    // // (define b 20) == b
-    // exp = list('define, 'b, 20)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Sym('b))
+    // (define b 20) == b
+    exp = list('define, 'b, 20)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Sym('b))
 
-    // // a == 10
-    // exp = 'a
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(10))
+    // a == 10
+    exp = 'a
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(10))
 
-    // // 100 == 100
-    // exp = 100
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(100))
+    // 100 == 100
+    exp = 100
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(100))
 
-    // // (lambda (a) a) == (lambda (a) a)
-    // exp = list('lambda, list('a), 'a)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Closure(createEnv(env), list('a), 'a))
+    // (lambda (a) a) == (lambda (a) a)
+    exp = list('lambda, list('a), 'a)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Closure(createEnv(env), list('a), 'a))
 
-    // // (define id (lambda (a) a)) == id
-    // var oldEnv = env;
-    // exp = list('define, 'id, list('lambda, list('a), 'a))
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Sym('id))
+    // (define id (lambda (a) a)) == id
+    exp = list('define, 'id, list('lambda, list('a), 'a))
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Sym('id))
 
-    // // id == (lambda (a) a)
-    // exp = 'id
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Closure(createEnv(oldEnv), list('a), 'a))
+    // id == (lambda (a) a)
+    exp = 'id
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Closure(createEnv(env), list('a), 'a))
 
-    // // (id 100) == 100
-    // exp = list('id, 100)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(100))
+    // (id 100) == 100
+    exp = list('id, 100)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(100))
 
     // built-in add
     val binaryAdd = BuiltIn { _ match {
@@ -211,120 +204,119 @@ class SispSpec extends FunSuite {
     env = set(env, Symbol("-"), binarySub)
     env = set(env, Symbol("*"), binaryMul)
 
-    // // (+ 100 200) == 300
-    // exp = list(Symbol("+"), Integer(100), Integer(200))
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(300))
+    // (+ 100 200) == 300
+    exp = list(Symbol("+"), Integer(100), Integer(200))
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(300))
 
-    // // (define x 100) == x
-    // exp = list('define, 'x, 100)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Sym('x))
+    // (define x 100) == x
+    exp = list('define, 'x, 100)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Sym('x))
 
-    // // (+ x 100) == 200
-    // exp = list('+, 100, 'x)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(200))
+    // (+ x 100) == 200
+    exp = list('+, 100, 'x)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(200))
 
-    // oldEnv = env
-    // // (define make-adder (lambda (a) (lambda (b) (+ a b))))
-    // exp = list('define, Symbol("make-adder"),
-    //   list('lambda, list('a),
-    //     list('lambda, list('b),
-    //       list('+, 'a, 'b))))
+    // (define make-adder (lambda (a) (lambda (b) (+ a b))))
+    exp = list('define, Symbol("make-adder"),
+      list('lambda, list('a),
+        list('lambda, list('b),
+          list('+, 'a, 'b))))
 
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Sym(Symbol("make-adder")))
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Sym(Symbol("make-adder")))
 
-    // // make-adder
-    // exp = Symbol("make-adder")
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
+    // make-adder
+    exp = Symbol("make-adder")
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value ==
+      Closure(
+        createEnv(env),
+        list('a),
+        list('lambda, list('b), list('+, 'a, 'b)))
+    )
+
+    val closureEnv = createEnv(env)
+    // (make-adder 10)
+    exp = list(Symbol("make-adder"), 10)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+
     // assert(value ==
     //   Closure(
-    //     createEnv(oldEnv),
-    //     list('a),
-    //     list('lambda, list('b), list('+, 'a, 'b)))
-    // )
+    //     createEnv(closureEnv),
+    //     list('b),
+    //     list('+, 'a, 'b)))
 
-    // val closureEnv = createEnv(oldEnv)
-    // // (make-adder 10)
-    // exp = list(Symbol("make-adder"), 10)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
+    // ((lambda (b) (+ a b)) 100) == 110
+    exp = list(value, 100)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(110))
 
-    // // assert(value ==
-    // //   Closure(
-    // //     Pair(closureEnv, Pair(Pair(Sym('a), Integer(10)), nil)),
-    // //     list('b),
-    // //     list('+, 'a, 'b)))
+    // (((lambda (a) (lambda (b) (+ a b))) 1) 2) == 3
+    exp =
+      list(
+        list(
+          list('lambda, list('a),
+            list('lambda, list('b),
+              list('+, 'a, 'b)
+            )),
+          1),
+        2)
 
-    // // ((lambda (b) (+ a b)) 100) == 110
-    // exp = list(value, 100)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(110))
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(3))
 
-    // // (((lambda (a) (lambda (b) (+ a b))) 1) 2) == 3
-    // exp =
-    //   list(
-    //     list(
-    //       list('lambda, list('a),
-    //         list('lambda, list('b),
-    //           list('+, 'a, 'b)
-    //         )),
-    //       1),
-    //     2)
+    // make build-in car function
+    val builtInCar = BuiltIn { _ match {
+      case Pair(hd, _) => hd
+      case `nil` => nil
+    }}
 
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(3))
+    val builtInCdr = BuiltIn { _ match {
+      case Pair(_, tl) => tl
+      case `nil` => nil
+    }}
 
-    // // make build-in car function
-    // val builtInCar = BuiltIn { _ match {
-    //   case Pair(hd, _) => hd
-    //   case `nil` => nil
-    // }}
+    // set built-in functions
+    exp = list('define, 'car, builtInCar)
+    ret = eval(env, exp)
+    env = car(ret)
 
-    // val builtInCdr = BuiltIn { _ match {
-    //   case Pair(_, tl) => tl
-    //   case `nil` => nil
-    // }}
+    exp = list('define, 'cdr, builtInCdr)
+    ret = eval(env, exp)
+    env = car(ret)
 
-    // // set built-in functions
-    // exp = list('define, 'car, builtInCar)
-    // ret = eval(env, exp)
-    // env = car(ret)
+    exp = list('car, 10, 20)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == Integer(10))
 
-    // exp = list('define, 'cdr, builtInCdr)
-    // ret = eval(env, exp)
-    // env = car(ret)
+    exp = list('cdr, 10, 20)
+    ret = eval(env, exp)
+    env = car(ret)
+    value = cdr(ret)
+    assert(value == list(Integer(20)))
 
-    // exp = list('car, 10, 20)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == Integer(10))
-
-    // exp = list('cdr, 10, 20)
-    // ret = eval(env, exp)
-    // env = car(ret)
-    // value = cdr(ret)
-    // assert(value == list(Integer(20)))
-
-    // env = set(env, 't, 't)
+    env = set(env, 't, 't)
 
     // t == true
     exp = Symbol("t")
